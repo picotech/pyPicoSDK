4840894fd8339b69a1a4e0b2451a507439913711
JamesPicoTech <118216728+JamesPicoTech@users.noreply.github.com>
2025-03-27 11:40:40 +0000

    Moved from bdist_wheel to cibuildwheel

--- build-tools/build_whl.bat
+++ build-tools/build_whl.bat
@@ -1,7 +1,10 @@
 @echo off
+cd build-tools
 echo updating versions from version_main.py
 python version_updater.py
+cd ..
 echo copying contents of index.md to README.md
 python -c "import shutil; shutil.copyfile('./docs/docs/index.md', './README.md')"
-python setup.py clean --all bdist_wheel
+cibuildwheel --output-dir dist
 echo twine upload --repository testpypi dist/*
+cd build-tools
