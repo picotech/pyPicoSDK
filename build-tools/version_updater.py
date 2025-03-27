61497d6fca273ee85de0e5043d8a97a616c94771
JamesPicoTech <118216728+JamesPicoTech@users.noreply.github.com>
2025-03-27 09:24:08 +0000

    Moved build tools to build-tools folder

--- build-tools/version_updater.py
+++ build-tools/version_updater.py
@@ -1,3 +1,7 @@
+import sys
+import os
+parent_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
+sys.path.append(parent_dir)
 from version import docs_version, package_version
 
 
