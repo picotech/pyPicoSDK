72feaf82ff894c9b260d88eff1592f7014bc56e3
JamesPicoTech <118216728+JamesPicoTech@users.noreply.github.com>
2025-04-07 17:32:37 +0100

    Built_whl removes old whl folders

--- build-tools/build_whl.bat
+++ build-tools/build_whl.bat
@@ -3,8 +3,11 @@ cd build-tools
 echo updating versions from version_main.py
 python version_updater.py
 cd ..
+rmdir /s /q dist
+rmdir /s /q build
+rmdir /s /q pypicosdk.egg-info
 echo copying contents of index.md to README.md
 python -c "import shutil; shutil.copyfile('./docs/docs/index.md', './README.md')"
 cibuildwheel --output-dir dist
 echo twine upload --repository testpypi dist/*
-cd build-tools
+cd ..
