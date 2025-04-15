f2dd4d946ed36ab14b41a1147c8862343e5d2209
JamesPicoTech <118216728+JamesPicoTech@users.noreply.github.com>
2025-04-15 14:17:22 +0100

    Updated build_whl.bat

--- build-tools/build_whl.bat
+++ build-tools/build_whl.bat
@@ -1,13 +1,6 @@
 @echo off
-cd build-tools
 echo updating versions from version_main.py
-python version_updater.py
-cd ..
+python build-tools\version_updater.py
 rmdir /s /q dist
-rmdir /s /q build
-rmdir /s /q pypicosdk.egg-info
-echo copying contents of index.md to README.md
-python -c "import shutil; shutil.copyfile('./docs/docs/index.md', './README.md')"
-cibuildwheel --output-dir dist
-echo twine upload --repository testpypi dist/*
-cd ..
+python -m build --wheel
+twine upload dist/*
