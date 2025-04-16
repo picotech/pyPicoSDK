8695bd4326ec158985575b81d93460784b0eb17f
James <118216728+JamesPicoTech@users.noreply.github.com>
2025-04-16 09:37:43 +0100

    Update example_6000a_advanced_block_capture.py

--- examples/example_6000a_advanced_block_capture.py
+++ examples/example_6000a_advanced_block_capture.py
@@ -7,34 +7,37 @@
 import pypicosdk as psdk
 from matplotlib import pyplot as plt
 
+# Setup variables
 timebase = 2
 samples = 100000
 channel = psdk.CHANNEL.A
 range = psdk.RANGE.V1
 
+# Initialise PicoScope
 scope = psdk.ps6000a()
-
 scope.open_unit()
-
 print(scope.get_unit_serial())
+
+# Setup channels and trigger
 scope.set_channel(channel=channel, range=range)
 scope.set_simple_trigger(channel=channel, threshold_mv=0) 
 
+# Run block capture and retrieve values
 channels_buffer = scope.set_data_buffer_for_enabled_channels(samples=samples)
 scope.run_block_capture(timebase=timebase, samples=samples)
 scope.get_values(samples)
 
-# No ADC conversion or time-axis yet
+# No ADC to mV conversion, add it here
 
+# Finish with PicoScope
 scope.close_unit()
 
-# Histogram
+# Build a Histogram of data
 plt.figure(0)
 plt.hist(channels_buffer[channel])
 plt.savefig('histogram_6000a.png')
 
-# Graph
+# Plot a graph of data
 plt.figure(1)
 plt.plot(channels_buffer[channel])
 plt.savefig('graph_6000a.png')
-
