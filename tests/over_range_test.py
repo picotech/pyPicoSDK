109a105e94e9d9d19541bd2f26707492f30486fb
JamesPicoTech <118216728+JamesPicoTech@users.noreply.github.com>
2025-05-23 11:35:41 +0100

    Added pytest debug mode for dll

--- tests/over_range_test.py
+++ tests/over_range_test.py
@@ -3,6 +3,6 @@ import warnings
 
 def test_adc_to_mv():
     warnings.simplefilter("ignore", OverrangeWarning)
-    scope = ps6000a()
+    scope = ps6000a('pytest')
     scope.over_range = 255
     assert scope.is_over_range() == ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H']
