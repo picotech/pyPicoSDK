608e7cfe1b2964fc92ecda43e656d7b7d2a8d01f
StuLawPico <stuart.law@picotech.com>
2025-07-08 09:29:26 +0100

    Revert "Enhance get_plot_range return type"

--- tests/plot_range_test.py
+++ tests/plot_range_test.py
@@ -5,15 +5,10 @@ from pypicosdk import RANGE, CHANNEL
 def test_plot_range_single_channel():
     scope = psdk.ps6000a('pytest')
     scope.range = {CHANNEL.A: RANGE.V1}
-    pr = scope.get_plot_range()
-    assert tuple(pr) == (-1000, 1000)
-    assert pr.ylim == (-1000, 1000)
-    assert pr.unit == "mV"
+    assert scope.get_plot_range() == (-1000, 1000)
 
 
 def test_plot_range_multiple_channels():
     scope = psdk.ps6000a('pytest')
     scope.range = {CHANNEL.A: RANGE.V1, CHANNEL.B: RANGE.V10}
-    pr = scope.get_plot_range()
-    assert tuple(pr) == (-10000, 10000)
-    assert pr.unit == "mV"
+    assert scope.get_plot_range() == (-10000, 10000)
