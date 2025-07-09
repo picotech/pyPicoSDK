1c731962d3961934c5ad058975dc084018cd8b8b
JamesPicoTech <118216728+JamesPicoTech@users.noreply.github.com>
2025-07-09 09:39:38 +0100

    Merged simple_block_capture example
    - Moved Timebase to after channel selection
    - Renamed siggen to simple block capture

--- examples/example_6000a_simple_block_capture.py
+++ examples/example_6000a_simple_block_capture.py
@@ -4,23 +4,29 @@ from matplotlib import pyplot as plt
 # Setup variables
 samples = 50_000
 channel_a = psdk.CHANNEL.A
-channel_b = psdk.CHANNEL.B
 range = psdk.RANGE.V1
+
+# SigGen variables
+frequency = 100_000
+pk2pk = 0.8
+wave_type = psdk.WAVEFORM.SINE
 
 # Initialise PicoScope 6000
 scope = psdk.ps6000a()
 scope.open_unit()
+
+# Setup siggen
+scope.set_siggen(frequency, pk2pk, wave_type)
+
+# Setup channels and trigger
+scope.set_channel(channel=channel_a, range=range)
+scope.set_simple_trigger(channel=channel_a, threshold_mv=0)
 
 # Set capture timebase
 TIMEBASE = scope.sample_rate_to_timebase(sample_rate=500,
                                          unit=psdk.SAMPLE_RATE.MSPS)
 # TIMEBASE = 2  # direct driver timebase
 # TIMEBASE = scope.interval_to_timebase(20E-9)
-
-# Setup channels and trigger
-scope.set_channel(channel=channel_a, range=range)
-scope.set_channel(channel=channel_b, range=range)
-scope.set_simple_trigger(channel=channel_a, threshold_mv=0)
 
 # Run the block capture
 channel_buffer, time_axis = scope.run_simple_block_capture(
@@ -31,13 +37,11 @@ channel_buffer, time_axis = scope.run_simple_block_capture(
 scope.close_unit()
 
 # Plot data to pyplot
-plt.plot(time_axis, channel_buffer[channel_a], label='Channel A')
-plt.plot(time_axis, channel_buffer[channel_b], label='Channel B')
+plt.plot(time_axis, channel_buffer[channel_a])
 
 # Add labels to pyplot
 plt.xlabel("Time (\u03bcs)")
 plt.ylabel("Amplitude (mV)")
 plt.ylim(scope.get_plot_range())
-plt.legend()
 plt.grid(True)
 plt.show()
