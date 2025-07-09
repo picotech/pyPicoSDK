b6e0661f4b438893ffc7ce75e9dc5ce3e3d2ac5c
JamesPicoTech <118216728+JamesPicoTech@users.noreply.github.com>
2025-07-09 11:52:25 +0100

    Upversioned to v0.3.0

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ extra_files = package_files('pypicosdk/lib')
 
 setup(
     name="pypicosdk",
-    version="0.2.30",
+    version="0.3.0",
     packages=find_packages(),
     include_package_data=True,
     has_ext_modules=lambda : True,
--- version.py
+++ version.py
@@ -1,4 +1,4 @@
 # Master version file for all scripts.
 # Update these variables and run a build-tool (which will run version_updater.py)
 docs_version = "0.1.6"
-package_version = "0.2.30"
+package_version = "0.3.0"
