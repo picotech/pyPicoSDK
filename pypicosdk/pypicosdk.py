dcc978e5127da9dbb9cb37f45631a131f8b7c57a
JamesPicoTech <118216728+JamesPicoTech@users.noreply.github.com>
2025-07-23 13:04:28 +0100

    Added _export_to_csv to pypicosdk.py

--- pypicosdk/pypicosdk.py
+++ pypicosdk/pypicosdk.py
@@ -1,6 +1,8 @@
 from . import constants as _constants
 from .constants import *
 from .version import VERSION
+import csv
+import numpy as np
 
 from .base import (
     PicoSDKNotFoundException,
@@ -22,6 +24,38 @@ def get_all_enumerated_units() -> tuple[int, list[str]]:
         unit_serial += units[1].split(',')
     return n_units, unit_serial
 
+def _export_to_csv_rapid(filename, channels_buffer, time_axis=None, time_unit='ns'):
+    headers = []
+    no_of_samples = len(channels_buffer[0][0])
+    no_of_captures = len(channels_buffer[0])
+
+    if time_axis != None:
+        headers.append(f'time ({time_unit})')
+    for channel in channels_buffer:
+        channel_name = list(channel_map.keys())[channel].title()
+        for column in range(len(channels_buffer[channel])):
+            headers.append(f'{channel_name}-{column+1}')
+        
+    with open(filename, 'w', newline='') as csvfile:
+        csv_writer = csv.writer(csvfile)
+        csv_writer.writerow(headers)
+        for sample_number in range(no_of_samples):
+            row = []
+            if time_axis != None:
+                row.append(time_axis[sample_number])
+            for channel in channels_buffer:
+                for n in range(no_of_captures):
+                    row.append(channels_buffer[channel][n][sample_number])
+            csv_writer.writerow(row)
+
+def export_to_csv(filename:str, channels_buffer:dict, time_axis:list=None):
+    if '.csv' not in filename: filename += '.csv'
+    if type(channels_buffer[0]) == list:
+        _export_to_csv_rapid(filename, channels_buffer, time_axis)
+    elif type(channels_buffer[0]) == np.array:
+        NotImplementedError('This data is not yet supported for export')
+    else: 
+        NotImplementedError('This data is not supported for export')
 
 __all__ = list(_constants.__all__) + [
     'PicoSDKNotFoundException',
@@ -29,6 +63,7 @@ __all__ = list(_constants.__all__) + [
     'OverrangeWarning',
     'PowerSupplyWarning',
     'get_all_enumerated_units',
+    'export_to_csv',
     'PicoScopeBase',
     'ps6000a',
 ]
