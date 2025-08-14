e6eda091191440399be7548c7daf34c002d79d54
JamesPicoTech <118216728+JamesPicoTech@users.noreply.github.com>
2025-08-14 16:12:46 +0100

    Added 3 psospa LED examples

---
+++ examples/psospa_led/breathing_leds.py
@@ -0,0 +1,26 @@
+"""
+This is an example of using the LED channel identification
+on the PSOSPA devices i.e. 3000E.
+
+To exit the loop, use Ctrl+Z KeyboardInterrupt, or exit the
+terminal.
+"""
+
+import pypicosdk as psdk
+import time
+
+scope = psdk.psospa()
+scope.open_unit()
+
+scope.set_all_led_states('on')
+scope.set_all_led_colours('blue')
+
+brightness = 50
+inc = 2
+
+while True:
+    scope.set_led_brightness(brightness)
+    if brightness in [0, 100]:
+        inc = -inc
+    brightness += inc
+    time.sleep(.1)
---
+++ examples/psospa_led/led_sweep.py
@@ -0,0 +1,30 @@
+"""
+This is an example of using the LED channel identification
+on the PSOSPA devices i.e. 3000E.
+
+To exit the loop, use Ctrl+Z KeyboardInterrupt, or exit the
+terminal.
+"""
+
+import pypicosdk as psdk
+import time
+
+scope = psdk.psospa()
+scope.open_unit()
+
+scope.set_all_led_states('off')
+scope.set_all_led_colours('red')
+
+led_list = ['A', 'B', 'C', 'D', 'AUX', 'AWG']
+sweep = ['off'] * 6
+n_sweep = 1
+sweep_inc = 1
+
+while True:
+    sweep[n_sweep] = 'on'
+    scope.set_led_states(led_list, sweep)
+    time.sleep(.1)
+    sweep[n_sweep] = 'off'
+    if n_sweep in [0, 5]:
+        sweep_inc = -sweep_inc
+    n_sweep += sweep_inc
