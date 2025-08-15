d56216bfcf7b19d7723a77a76f32d93da83cdf37
JamesPicoTech <118216728+JamesPicoTech@users.noreply.github.com>
2025-08-15 12:40:33 +0100

    Updated build_docs.py  - Enhanced docstrings  - Added debug info

--- build-tools/build_docs.py
+++ build-tools/build_docs.py
@@ -13,12 +13,16 @@ if not os.path.exists('docs'):
     raise FileNotFoundError('docs/ does not exist in this directory')
 
 ref_dir = 'docs/docs/ref/'
-copy_dir = 'psospa'
+copy_dir = 'psospa' # Main copy dir (truth source)
+
+# Paste directory list with an exclude for certain files
 paste_dir_list = [
-    'ps6000a'
+    {'name': 'ps6000a', 'exclude': ['led.md',]}
 ]
 
+# List of files to copy from copy_dir to paste_dir
 file_list = [
+    # 'init.md', Do not include init.md
     'buffers.md',
     'captures.md', 
     'channel.md',
@@ -30,11 +34,23 @@ file_list = [
     'setup.md',
     'siggen.md',
     'trigger.md',
+    'led.md',
 ]
 
-for paste_dir in paste_dir_list:
+# Report any files that aren't included for info
+all_files = os.listdir(os.path.join(ref_dir, copy_dir))
+missing_files = [f for f in all_files if f not in file_list]
+print('Files not included in copy:', missing_files)
+
+# Copy each file to each paste_dir 
+for paste_dir_dict in paste_dir_list:
+    paste_dir = paste_dir_dict['name']
+    exclude_list = paste_dir_dict['exclude']
     for f in file_list:
+        if f in exclude_list:
+            continue
         copy_fp = os.path.join(ref_dir, copy_dir, f)
         paste_fp = os.path.join(ref_dir, paste_dir, f)
+        print(f'Copying {f} to {paste_fp}')
         with open(copy_fp) as f: text = f.read()
         with open(paste_fp, 'w') as f: f.write(text.replace(copy_dir, paste_dir))
