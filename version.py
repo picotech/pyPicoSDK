5063d679a54544277f95c12e14ee63424332924e
JamesPicoTech <118216728+JamesPicoTech@users.noreply.github.com>
2025-08-18 11:00:17 +0100

    Upversioned to 1.3.1

--- version.py
+++ version.py
@@ -1,4 +1,4 @@
 # Master version file for all scripts.
 # Update these variables and run a build-tool (which will run version_updater.py)
 docs_version = "0.4.0"
-package_version = "1.3.0"
+package_version = "1.3.1"
