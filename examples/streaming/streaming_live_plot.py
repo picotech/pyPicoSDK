3559f10d0f928ddecd9784eb1921203958db50d5
JamesPicoTech <118216728+JamesPicoTech@users.noreply.github.com>
2025-08-27 08:48:45 +0100

    Updated streaming  - Updated streaming.py  - Updated streaming_live_plot.py

--- examples/streaming/streaming_live_plot.py
+++ examples/streaming/streaming_live_plot.py
@@ -1,68 +1,81 @@
 """
-Note: RACE CONDITION!
-The data retrieved from get_streaming data *NEEDS* to be 
-    larger than the captured data. 
-    If it's smaller, the live plot will lag behind the data. 
-    If it's larger, the streaming will ignore the empty buffers.
+This is an example of streaming mode displayed to a pyplot animation plot.
 
-Threading: 
-    The main streaming is handled in a thread while loop.
-    This is so that the matplotlib animation can be the main loop,
-    so the frame update interval doesn't control the retrieval of data
-    removing any race condition from the animation.
+To do this it uses the StreamingScope class from pypicosdk.streaming
+and runs a thread containing the streaming loop.
 """
 
+import threading
+import numpy as np
+
+from matplotlib import pyplot as plt
+from matplotlib.animation import FuncAnimation
 
 import pypicosdk as psdk
 from pypicosdk.streaming import StreamingScope
-from matplotlib import pyplot as plt
-from matplotlib.animation import FuncAnimation
-import numpy as np
-import threading
-import time
 
 # Capture configuration
-samples = int(1E9)
-streaming_samples = 250
-interval = 20
-display_samples = 1000
+SAMPLES = int(1E6)  # Maxmimum samples in scope memory
+DISPLAY_SAMPLES = 10_000  # Samples of pyplot x-axis
+DISPLAY_RATIO = SAMPLES // DISPLAY_SAMPLES  # Downsample ratio for display
+
+INTERVAL = 10
 pico_unit = psdk.PICO_TIME_UNIT.NS
 channel = psdk.CHANNEL.A
 
 
-fig = plt.figure() 
-axis = plt.axes(xlim =(0, display_samples), 
-                ylim =(-32000, 32000)) 
-x = np.arange(display_samples)  # Predefined x-data of length 10
-line, = axis.plot(x, np.zeros_like(x), lw=2)  # Initialize with zeros
+def setup_pyplot():
+    """Sets up pyplot with empty data"""
+    fig = plt.figure()
+    axis = plt.axes(xlim=(0, DISPLAY_SAMPLES),
+                    ylim=(-32000, 32000))
+    x = np.arange(DISPLAY_SAMPLES)  # Predefined x-data of length 10
+    line, = axis.plot(x, np.zeros_like(x), lw=2)  # Initialize with zeros
+    return fig, line
 
 
 def setup_scope():
-    scope = psdk.ps6000a()
+    """Setup PicoScope and get scope class"""
+    scope = psdk.psospa()
     scope.open_unit()
-    scope.set_siggen(frequency=1_000_000, pk2pk=1.6, wave_type=psdk.WAVEFORM.TRIANGLE)
+    scope.set_siggen(frequency=10_000, pk2pk=1.6,
+                     wave_type=psdk.WAVEFORM.TRIANGLE)
     scope.set_channel(channel=psdk.CHANNEL.A, range=psdk.RANGE.V1)
     scope.set_simple_trigger(channel=psdk.CHANNEL.A, threshold_mv=0)
     return scope
 
-def streaming_thread(stream:StreamingScope):
-    stream.run_streaming_while()
 
-def animate(frame, stream:StreamingScope):
-    data = stream.buffer_array
+def streaming_thread(stream: StreamingScope):
+    """Streaming thread"""
+    stream.start_streaming_while()
+
+
+def animate(_, line, stream: StreamingScope):
+    """pyplot animation thread"""
+
+    # Print streaming rate in MS/s
+    print(f'Min/Max/Avg: {stream.msps_min}, {stream.msps_max}'
+          f', {stream.msps_avg} MS/s')
+
+    # Get streaming data and update pyplot
+    data = stream.buffer[::DISPLAY_RATIO]
     line.set_ydata(data)
-    return line,
+    return [line]
+
 
 def main():
-    stop = False
+    """Main function"""
+    fig, line = setup_pyplot()
+
     scope = setup_scope()
     stream = StreamingScope(scope)
-    stream.config_streaming(channel, samples, interval, pico_unit, display_samples)
+    stream.config_streaming(channel, SAMPLES, INTERVAL, pico_unit)
 
     th = threading.Thread(target=streaming_thread, args=[stream])
     th.start()
 
-    anim = FuncAnimation(fig, animate, frames=500, fargs=(stream, ), interval=20, blit=True)
+    _ = FuncAnimation(fig, animate, frames=500, fargs=(line, stream, ),
+                      interval=20, blit=True)
     plt.show()
 
     stream.stop()
