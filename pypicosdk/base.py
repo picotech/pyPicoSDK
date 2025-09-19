e71071f7f5d31c2a0d081a2c11a079c9b6e8cf45
JamesPicoTech <118216728+JamesPicoTech@users.noreply.github.com>
2025-09-19 11:16:31 +0100

    Added noqa to legacy files.

--- pypicosdk/base.py
+++ pypicosdk/base.py
@@ -2,6 +2,8 @@
 Copyright (C) 2025-2025 Pico Technology Ltd. See LICENSE file for terms.
 """
 
+# flake8: noqa
+# pylint: skip-file
 import ctypes
 import os
 import warnings
--- pypicosdk/psospa.py
+++ pypicosdk/psospa.py
@@ -1,5 +1,7 @@
 """Copyright (C) 2025-2025 Pico Technology Ltd. See LICENSE file for terms."""
 
+# flake8: noqa
+# pylint: skip-file
 import ctypes
 from typing import override, Literal
 import json
