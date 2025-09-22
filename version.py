2abb842b2c93ed3d6591d3649119e062721bf8b7
JamesPicoTech <118216728+JamesPicoTech@users.noreply.github.com>
2025-09-22 10:00:23 +0100

    Upversioned to 1.6.2

--- version.py
+++ version.py
@@ -2,4 +2,4 @@
 # Master version file for all scripts.
 # Update these variables and run a build-tool (which will run version_updater.py)
 docs_version = "0.4.0"
-package_version = "1.6.1"
+package_version = "1.6.2"
