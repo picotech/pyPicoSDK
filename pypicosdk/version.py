79126640f419e01b2f7ced051eb1ebb8bd4f9d8e
JamesPicoTech <118216728+JamesPicoTech@users.noreply.github.com>
2025-10-23 15:55:18 +0100

    Updated version - Updated `VERSION` to `__version__` in python - Upversioned to 1.7.0

--- pypicosdk/version.py
+++ pypicosdk/version.py
@@ -1,2 +1,2 @@
 """Copyright (C) 2025-2025 Pico Technology Ltd. See LICENSE file for terms."""
-VERSION = "1.6.2"
+__version__ = "1.7.0"
--- version.py
+++ version.py
@@ -2,4 +2,4 @@
 # Master version file for all scripts.
 # Update these variables and run a build-tool (which will run version_updater.py)
 docs_version = "0.4.1"
-package_version = "1.6.2"
+package_version = "1.7.0"
